c39caf2a3067cc6da232e0fe756a15a0a3636407
mcorcias <mosescorcias@gmail.com>
2019-11-05 23:14:05 +0200

    added new routes

--- src/app.js
+++ src/app.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 const path = require('path');
-
+const bodyParser = require('body-parser');
+const app = express();
 const routes = require('./routes/index');
 //const helpers = require('./views/helpers/index');
 
+//app.use(express.static(path.join(__dirname, '..', 'public')));
 
-const app = express();
+app.use('/public', express.static('public'));
+
+// parse incoming json
+app.use(bodyParser.json());
+// parse urlencoded bodies
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
